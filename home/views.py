4c099829ee3bd34500516b01f07d1fad8dcbf994
dogancankilment <dogancankilment@gmail.com>
2018-12-13 01:33:45 -0800

    yapilanlar

--- home/views.py
+++ home/views.py
@@ -7,8 +7,183 @@ import feedparser
 import pandas as pd
 
 
-def test(request):
+def hasta_goruntule(request):
     c = {"request": request}
     c.update(csrf(request))
 
-    return render(request, "home/excel.html", c)
+    return render(request, "goruntule/hasta_goruntule.html", c)
+
+
+def hastane_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/hastane_goruntule.html", c)
+
+
+def doktor_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/doktor_goruntule.html", c)
+
+
+def idarikadro_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/idarikadro_goruntule.html", c)
+
+
+def kullanici_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/kullanici_goruntule.html", c)
+
+
+def hastalik_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/hastalik_goruntule.html", c)
+
+
+def teshis_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/teshis_goruntule.html", c)
+
+
+def ilac_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/ilac_goruntule.html", c)
+
+
+def recete_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/recete_goruntule.html", c)
+
+
+def randevu_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/randevu_goruntule.html", c)
+
+
+def klinik_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/klinik_goruntule.html", c)
+
+
+def yonetici_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/yonetici_goruntule.html", c)
+
+
+def izinler_goruntule(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "goruntule/izinler_goruntule.html", c)
+
+
+def yeni_hasta(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_hasta.html", c)
+
+
+def yeni_hastane(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_hastane.html", c)
+
+
+def yeni_doktor(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_doktor.html", c)
+
+
+def yeni_idarikadro(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_idarikadro.html", c)
+
+
+def yeni_kullanici(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_kullanici.html", c)
+
+
+def yeni_hastalik(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_hastalik.html", c)
+
+
+def yeni_teshis(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_teshis.html", c)
+
+
+def yeni_ilac(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_ilac.html", c)
+
+
+def yeni_recete(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_recete.html", c)
+
+
+def yeni_randevu(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_randevu.html", c)
+
+
+def yeni_klinik(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_klinik.html", c)
+
+
+def yeni_yonetici(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_yonetici.html", c)
+
+
+def yeni_izinler(request):
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render(request, "yeni/yeni_izinler.html", c)
--- hospitalautomation/urls.py
+++ hospitalautomation/urls.py
@@ -5,9 +5,109 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$',
-                           'home.views.test',
-                           name='test'),
+                       url(r'^hasta_goruntule$',
+                           'home.views.hasta_goruntule',
+                           name='hasta_goruntule'),
+
+                       url(r'^hastane_goruntule$',
+                           'home.views.hastane_goruntule',
+                           name='hastane_goruntule'),
+
+                       url(r'^doktor_goruntule$',
+                           'home.views.doktor_goruntule',
+                           name='doktor_goruntule'),
+
+                       url(r'^idarikadro_goruntule$',
+                           'home.views.idarikadro_goruntule',
+                           name='idarikadro_goruntule'),
+
+                       url(r'^kullanici_goruntule$',
+                           'home.views.kullanici_goruntule',
+                           name='kullanici_goruntule'),
+
+                       url(r'^hastalik_goruntule$',
+                           'home.views.hastalik_goruntule',
+                           name='hastalik_goruntule'),
+
+                       url(r'^teshis_goruntule$',
+                           'home.views.teshis_goruntule',
+                           name='teshis_goruntule'),
+
+                       url(r'^ilac_goruntule$',
+                           'home.views.ilac_goruntule',
+                           name='ilac_goruntule'),
+
+                       url(r'^recete_goruntule$',
+                           'home.views.recete_goruntule',
+                           name='recete_goruntule'),
+
+                       url(r'^randevu_goruntule$',
+                           'home.views.randevu_goruntule',
+                           name='randevu_goruntule'),
+
+                       url(r'^klinik_goruntule$',
+                           'home.views.klinik_goruntule',
+                           name='klinik_goruntule'),
+
+                       url(r'^yonetici_goruntule$',
+                           'home.views.yonetici_goruntule',
+                           name='yonetici_goruntule'),
+
+                       url(r'^izinler_goruntule$',
+                           'home.views.izinler_goruntule',
+                           name='izinler_goruntule'),
+
+                       url(r'^yeni_hasta$',
+                           'home.views.yeni_hasta',
+                           name='yeni_hasta'),
+
+                       url(r'^yeni_hastane$',
+                           'home.views.yeni_hastane',
+                           name='yeni_hastane'),
+
+                       url(r'^yeni_doktor$',
+                           'home.views.yeni_doktor',
+                           name='yeni_doktor'),
+
+                       url(r'^yeni_idarikadro$',
+                           'home.views.yeni_idarikadro',
+                           name='yeni_idarikadro'),
+
+                       url(r'^yeni_kullanici$',
+                           'home.views.yeni_kullanici',
+                           name='yeni_kullanici'),
+
+                       url(r'^yeni_hastalik$',
+                           'home.views.yeni_hastalik',
+                           name='yeni_hastalik'),
+
+                       url(r'^yeni_teshis$',
+                           'home.views.yeni_teshis',
+                           name='yeni_teshis'),
+
+                       url(r'^yeni_ilac$',
+                           'home.views.yeni_ilac',
+                           name='yeni_ilac'),
+
+                       url(r'^yeni_recete$',
+                           'home.views.yeni_recete',
+                           name='yeni_recete'),
+
+                       url(r'^yeni_randevu$',
+                           'home.views.yeni_randevu',
+                           name='yeni_randevu'),
+
+                       url(r'^yeni_klinik$',
+                           'home.views.yeni_klinik',
+                           name='yeni_klinik'),
+
+                       url(r'^yeni_yonetici$',
+                           'home.views.yeni_yonetici',
+                           name='yeni_yonetici'),
+
+                       url(r'^yeni_izinler$',
+                           'home.views.yeni_izinler',
+                           name='yeni_izinler'),
 
                        # url(r'^hospitalautomation/',
                        #     include('hospitalautomation.foo.urls')),
